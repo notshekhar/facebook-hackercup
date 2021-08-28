70ba4552a72ae3c74f656193f99cc18724547559
Shekhar Tyagi <notshekhar@gmail.com>
2021-08-28 12:55:38 +0530

    new

--- Qualification Round/Chapter 1/A1 Chapter 1.py
+++ Qualification Round/Chapter 1/A1 Chapter 1.py
@@ -11,25 +11,34 @@ def solution():
     set_consonants = set()
     total_vowels = 0
     total_consonants = 0
-    dist = {}
+    v_dist = {}
+    c_dist = {}
 
     for i in range(str_len):
         s = string[i]
         
-        if(s in dist): dist[s] += 1
-        else: dist[s] = 0
+        
 
         if(s in vowels):
+            if(s in v_dist): v_dist[s] += 1
+            else: v_dist[s] = 0
             if(not s in set_vowel): n_vowels += 1
             total_vowels += 1
             set_vowel.add(s)
             continue
+
+        if(s in c_dist): c_dist[s] += 1
+        else: c_dist[s] = 0
         if(not s in set_consonants): n_consonants += 1
         total_consonants += 1
         set_consonants.add(s)
     
-    return dist 
+    if(n_vowels == 1): return total_consonants
+    if(n_consonants == 1): return total_vowels
+    if(n_vowels == 0 or n_consonants == 0): return str_len
 
+    _min  = min(n_vowels, n_consonants)
+    return str_len
 for t in range(N):
     print("Case #{}".format(t+1), solution())
 
