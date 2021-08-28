564567bbc154431f6b572115345b638f03e4dc07
Shekhar Tyagi <notshekhar@gmail.com>
2021-08-28 13:19:29 +0530

    new

--- Qualification Round/Chapter 1/A1 Chapter 1.py
+++ Qualification Round/Chapter 1/A1 Chapter 1.py
@@ -21,14 +21,14 @@ def solution():
 
         if(s in vowels):
             if(s in v_dist): v_dist[s] += 1
-            else: v_dist[s] = 0
+            else: v_dist[s] = 1
             if(not s in set_vowel): n_vowels += 1
             total_vowels += 1
             set_vowel.add(s)
             continue
 
         if(s in c_dist): c_dist[s] += 1
-        else: c_dist[s] = 0
+        else: c_dist[s] = 1
         if(not s in set_consonants): n_consonants += 1
         total_consonants += 1
         set_consonants.add(s)
@@ -37,8 +37,35 @@ def solution():
     if(n_consonants == 1): return total_vowels
     if(n_vowels == 0 or n_consonants == 0): return str_len
 
-    _min  = min(n_vowels, n_consonants)
-    return str_len
+    v_max_f = 0
+    c_max_f = 0
+    for key in v_dist:
+        freq = v_dist[key]
+        v_max_f = max(v_max_f, freq)
+
+    for key in c_dist:
+        freq = c_dist[key]
+        c_max_f = max(c_max_f, freq)
+
+    cal_vowel = total_vowels - v_max_f
+    cal_consonant = total_consonants - c_max_f
+
+    total_min = 0
+    total_max = 0
+    max_f = 0
+
+    if(cal_vowel < cal_consonant):
+        max_f = v_max_f
+        total_min = total_vowels
+        total_max = total_consonants
+    else: 
+        max_f = c_max_f
+        total_min = total_consonants
+        total_max = total_vowels
+
+    return (2*(total_min-max_f)) + total_max
+
+
 for t in range(N):
-    print("Case #{}".format(t+1), solution())
+    print("Case #{}:".format(t+1), solution())
 
